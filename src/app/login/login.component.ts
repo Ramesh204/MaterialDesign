5325afb05a0f87cd280c6d989fbc342f8f395224
Anantavarapu <Ramesh.Anantavarapu@CenturyLink.com>
2018-12-10 16:00:08 +0530

    login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -9,7 +10,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class LoginComponent implements OnInit {
 
   loginFormGroup : FormGroup;
-  constructor(private formBuilder:FormBuilder) { }
+  constructor(private formBuilder:FormBuilder,private authService:AuthService) { }
 
   ngOnInit() {
     this.loginFormGroup = this.formBuilder.group({
